181d9ae03642548e572b3f1afc97abe3369d7837
Julio Estrada <juliojr281001@gmail.com>
2023-11-21 21:34:34 -0600

    Look for a instagram profile

--- instagramBot.py
+++ instagramBot.py
@@ -24,3 +24,15 @@ class InstagramBot:
     self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(os.getenv('PASSWORD'))
     time.sleep(5)
     self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').click()
+    
+  
+  def find_account(self, account):
+    
+    time.sleep(5)
+    self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a/div').click()
+    time.sleep(5)
+    self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input').send_keys(account)
+    time.sleep(5)
+    self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/a[1]/div[1]/div/div/div[2]/div/div').click()
+    time.sleep(10)
+    self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[3]/a').click()
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from instagramBot import InstagramBot
 bot = InstagramBot()
 
 bot.sign_in()
+bot.find_account('programmingknow')
